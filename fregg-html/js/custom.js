a0c3650c342f66bb10a6d3a19ce38800f154ab9d
Muhammad Firdaus Bin Mohd Fauzi <ffdausfauzi8@gmail.com>
2024-02-19 23:31:55 +0800

    Initial commit

---
+++ fregg-html/js/custom.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    // Smooth scrolling for all links
+    $("a").on('click', function (event) {
+       // Make sure this.hash has a value before overriding default behavior
+       if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+          // Store hash
+          var hash = this.hash;
+          // Using jQuery's animate() method to add smooth page scroll
+          $('html, body').animate({
+             scrollTop: $(hash).offset().top
+          }, 800, function () {
+             // Add hash (#) to URL when done scrolling (default click behavior)
+             window.location.hash = hash;
+          });
+       } // End if
+    });
+ 
+    // Smooth scrolling specifically for the "Home" link
+    $(".navbar-nav .nav-item.active a, .navbar-brand").on('click', function (event) {
+       // Make sure this.hash has a value before overriding default behavior
+       if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+          // Store hash
+          var hash = this.hash;
+          // Using jQuery's animate() method to add smooth page scroll
+          $('html, body').animate({
+             scrollTop: $(hash).offset().top
+          }, 800, function () {
+             // Add hash (#) to URL when done scrolling (default click behavior)
+             window.location.hash = hash;
+          });
+       } // End if
+    });
+ });
+ window.onscroll = function () {
+    stickyNavbar();
+  };
+  
+  var header = document.querySelector(".header_section");
+  var sticky = header.offsetTop;
+  
+  function stickyNavbar() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("fixed-top");
+    } else {
+      header.classList.remove("fixed-top");
+    }
+  }
